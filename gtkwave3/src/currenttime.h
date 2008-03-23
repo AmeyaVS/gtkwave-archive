3dbf51c0bf2fb2d9eccc55d1069ea53bd95ef44c
gtkwave <bybell@rocketmail.com>
2008-03-23 04:26:42 +0000

    promote to 3.1.7

--- gtkwave3/src/currenttime.h
+++ gtkwave3/src/currenttime.h
@@ -20,7 +20,7 @@
 #include "regex_wave.h"
 #include "translate.h"
 
-#define WAVE_VERSION_INFO "GTKWave Analyzer v3.1.7_pre (w)1999-2008 BSI"
+#define WAVE_VERSION_INFO "GTKWave Analyzer v3.1.7 (w)1999-2008 BSI"
 
 #define WAVE_INF_SCALING (0.5)
 
@@ -169,6 +169,9 @@ void do_popup_menu (GtkWidget *my_widget, GdkEventButton *event);
 /*
  * $Id$
  * $Log$
+ * Revision 1.30  2008/02/28 16:14:48  gtkwave
+ * starting with 3.1.7 changes
+ *
  * Revision 1.29  2008/02/26 04:57:51  gtkwave
  * version bump to 3.1.6
  *
