a045ad37ec88c5483295b7148dcda89308b352e8
gtkwave <bybell@rocketmail.com>
2010-06-04 20:26:12 +0000

    version bump

--- gtkwave3/src/currenttime.h
+++ gtkwave3/src/currenttime.h
@@ -20,7 +20,7 @@
 #include "regex_wave.h"
 #include "translate.h"
 
-#define WAVE_VERSION_INFO "GTKWave Analyzer v3.3.7_pre (w)1999-2010 BSI"
+#define WAVE_VERSION_INFO "GTKWave Analyzer v3.3.7 (w)1999-2010 BSI"
 
 #define WAVE_INF_SCALING (0.5)
 #define WAVE_SI_UNITS " munpfaz"
@@ -178,6 +178,9 @@ void do_popup_menu (GtkWidget *my_widget, GdkEventButton *event);
 /*
  * $Id$
  * $Log$
+ * Revision 1.76  2010/05/06 15:57:52  gtkwave
+ * enable_fast_exit now is enabled by default
+ *
  * Revision 1.75  2010/05/03 21:42:16  gtkwave
  * version bump
  *
