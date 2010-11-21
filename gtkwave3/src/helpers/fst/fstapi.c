c0e962b155f13f625b0c2dcb0a7d36b931ac3d0a
gtkwave <bybell@rocketmail.com>
2010-11-21 01:39:41 +0000

    added jenkins hash support

--- gtkwave3/src/helpers/fst/fstapi.c
+++ gtkwave3/src/helpers/fst/fstapi.c
@@ -27,6 +27,14 @@
 
 #ifdef _WAVE_HAVE_JUDY
 #include <Judy.h>
+#else
+typedef const void *Pcvoid_t;
+typedef void *Pvoid_t;
+typedef void **PPvoid_t;
+#define JudyHSIns(a,b,c,d) JenkinsIns((a),(b),(c))
+#define JudyHSFreeArray(a,b) JenkinsFree((a))
+void JenkinsFree(void *base_i);
+void **JenkinsIns(void *base_i, unsigned char *mem, uint32_t length);
 #endif
 
 #undef  FST_DEBUG
@@ -1031,10 +1039,7 @@ off_t unc_memreq = 0; /* for reader */
 unsigned char *packmem;
 unsigned int packmemlen;
 uint32_t *vm4ip;
-
-#ifdef _WAVE_HAVE_JUDY
 Pvoid_t PJHSArray = (Pvoid_t) NULL;
-#endif
 
 struct fstWriterContext *xc = (struct fstWriterContext *)ctx;
 
@@ -1185,7 +1190,6 @@ for(i=0;i<xc->maxhandle;i++)
 		        	rc = compress2(dmem, &destlen, scratchpnt, wrlen, 4);
 				if(rc == Z_OK)
 					{
-#ifdef _WAVE_HAVE_JUDY
 					PPvoid_t pv = JudyHSIns(&PJHSArray, dmem, destlen, NULL);
 					if(*pv)
 						{
@@ -1195,17 +1199,13 @@ for(i=0;i<xc->maxhandle;i++)
 						else
 						{
 						*pv = (void *)(long)(i+1);
-#endif
 						fpos += fstWriterVarint(f, wrlen);
 						fpos += destlen;
 						fstFwrite(dmem, destlen, 1, f);
-#ifdef _WAVE_HAVE_JUDY
 						}
-#endif
 					}
 					else
 					{
-#ifdef _WAVE_HAVE_JUDY
 					PPvoid_t pv = JudyHSIns(&PJHSArray, scratchpnt, wrlen, NULL);
 					if(*pv)
 						{
@@ -1215,13 +1215,10 @@ for(i=0;i<xc->maxhandle;i++)
 						else
 						{
 						*pv = (void *)(long)(i+1);
-#endif
 						fpos += fstWriterVarint(f, 0);
 						fpos += wrlen;
 						fstFwrite(scratchpnt, wrlen, 1, f);
-#ifdef _WAVE_HAVE_JUDY
 						}
-#endif
 					}
 				}
 				else
@@ -1239,7 +1236,6 @@ for(i=0;i<xc->maxhandle;i++)
 				rc = fastlz_compress(scratchpnt, wrlen, dmem);
 				if(rc < destlen)
         				{
-#ifdef _WAVE_HAVE_JUDY
 					PPvoid_t pv = JudyHSIns(&PJHSArray, dmem, rc, NULL);
 					if(*pv)
 						{
@@ -1249,17 +1245,13 @@ for(i=0;i<xc->maxhandle;i++)
 						else
 						{
 						*pv = (void *)(long)(i+1);
-#endif
 						fpos += fstWriterVarint(f, wrlen);
 						fpos += rc;
 						fstFwrite(dmem, rc, 1, f);
-#ifdef _WAVE_HAVE_JUDY
 						}
-#endif
         				}
         				else
         				{
-#ifdef _WAVE_HAVE_JUDY
 					PPvoid_t pv = JudyHSIns(&PJHSArray, scratchpnt, wrlen, NULL);
 					if(*pv)
 						{
@@ -1269,19 +1261,15 @@ for(i=0;i<xc->maxhandle;i++)
 						else
 						{
 						*pv = (void *)(long)(i+1);
-#endif
 						fpos += fstWriterVarint(f, 0);
 						fpos += wrlen;
 						fstFwrite(scratchpnt, wrlen, 1, f);
-#ifdef _WAVE_HAVE_JUDY
 						}
-#endif
         				}
 				}
 			}
 			else
 			{
-#ifdef _WAVE_HAVE_JUDY
 			PPvoid_t pv = JudyHSIns(&PJHSArray, scratchpnt, wrlen, NULL);
 			if(*pv)
 				{
@@ -1291,13 +1279,10 @@ for(i=0;i<xc->maxhandle;i++)
 				else
 				{
 				*pv = (void *)(long)(i+1);
-#endif
 				fpos += fstWriterVarint(f, 0);
 				fpos += wrlen;
 				fstFwrite(scratchpnt, wrlen, 1, f);
-#ifdef _WAVE_HAVE_JUDY
 				}
-#endif
 			}
 
 		vm4ip[3] = 0;
@@ -1307,9 +1292,7 @@ for(i=0;i<xc->maxhandle;i++)
 		}
 	}
 
-#ifdef _WAVE_HAVE_JUDY
 JudyHSFreeArray(&PJHSArray, NULL);
-#endif
 free(packmem); packmem = NULL; packmemlen = 0;
 
 prevpos = 0; zerocnt = 0;
@@ -4518,3 +4501,211 @@ if(xc->signal_lens[facidx] == 1)
 
 /* return(NULL); */
 }
+
+
+
+/**********************************************************************/
+#ifndef _WAVE_HAVE_JUDY
+
+/***********************/
+/***                 ***/
+/***  jenkins hash   ***/
+/***                 ***/
+/***********************/
+
+#define J_HASHSIZE (20)
+
+#define hashsize(n) ((uint32_t)1<<(n))
+#define hashmask(n) (hashsize(n)-1)
+
+/*
+--------------------------------------------------------------------
+mix -- mix 3 32-bit values reversibly.
+For every delta with one or two bits set, and the deltas of all three
+  high bits or all three low bits, whether the original value of a,b,c
+  is almost all zero or is uniformly distributed,
+* If mix() is run forward or backward, at least 32 bits in a,b,c
+  have at least 1/4 probability of changing.
+* If mix() is run forward, every bit of c will change between 1/3 and
+  2/3 of the time.  (Well, 22/100 and 78/100 for some 2-bit deltas.)
+mix() was built out of 36 single-cycle latency instructions in a 
+  structure that could supported 2x parallelism, like so:
+      a -= b; 
+      a -= c; x = (c>>13);
+      b -= c; a ^= x;
+      b -= a; x = (a<<8);
+      c -= a; b ^= x;
+      c -= b; x = (b>>13);
+      ...
+  Unfortunately, superscalar Pentiums and Sparcs can't take advantage 
+  of that parallelism.  They've also turned some of those single-cycle
+  latency instructions into multi-cycle latency instructions.  Still,
+  this is the fastest good hash I could find.  There were about 2^^68
+  to choose from.  I only looked at a billion or so.
+--------------------------------------------------------------------
+*/
+#define mix(a,b,c) \
+{ \
+  a -= b; a -= c; a ^= (c>>13); \
+  b -= c; b -= a; b ^= (a<<8); \
+  c -= a; c -= b; c ^= (b>>13); \
+  a -= b; a -= c; a ^= (c>>12);  \
+  b -= c; b -= a; b ^= (a<<16); \
+  c -= a; c -= b; c ^= (b>>5); \
+  a -= b; a -= c; a ^= (c>>3);  \
+  b -= c; b -= a; b ^= (a<<10); \
+  c -= a; c -= b; c ^= (b>>15); \
+}
+
+/*
+--------------------------------------------------------------------
+j_hash() -- hash a variable-length key into a 32-bit value
+  k       : the key (the unaligned variable-length array of bytes)
+  len     : the length of the key, counting by bytes
+  initval : can be any 4-byte value
+Returns a 32-bit value.  Every bit of the key affects every bit of
+the return value.  Every 1-bit and 2-bit delta achieves avalanche.
+About 6*len+35 instructions.
+
+The best hash table sizes are powers of 2.  There is no need to do
+mod a prime (mod is sooo slow!).  If you need less than 32 bits,
+use a bitmask.  For example, if you need only 10 bits, do
+  h = (h & hashmask(10));
+In which case, the hash table should have hashsize(10) elements.
+
+If you are hashing n strings (uint8_t **)k, do it like this:
+  for (i=0, h=0; i<n; ++i) h = hash( k[i], len[i], h);
+
+By Bob Jenkins, 1996.  bob_jenkins@burtleburtle.net.  You may use this
+code any way you wish, private, educational, or commercial.  It's free.
+
+See http://burtleburtle.net/bob/hash/evahash.html
+Use for hash table lookup, or anything where one collision in 2^^32 is
+acceptable.  Do NOT use for cryptographic purposes.
+--------------------------------------------------------------------
+*/
+
+static uint32_t j_hash( k, length, initval)
+uint8_t *k;        /* the key */
+uint32_t  length;   /* the length of the key */
+uint32_t  initval;  /* the previous hash, or an arbitrary value */
+{
+   uint32_t a,b,c,len;
+
+   /* Set up the internal state */
+   len = length;
+   a = b = 0x9e3779b9;  /* the golden ratio; an arbitrary value */
+   c = initval;         /* the previous hash value */
+
+   /*---------------------------------------- handle most of the key */
+   while (len >= 12)
+   {
+      a += (k[0] +((uint32_t)k[1]<<8) +((uint32_t)k[2]<<16) +((uint32_t)k[3]<<24));
+      b += (k[4] +((uint32_t)k[5]<<8) +((uint32_t)k[6]<<16) +((uint32_t)k[7]<<24));
+      c += (k[8] +((uint32_t)k[9]<<8) +((uint32_t)k[10]<<16)+((uint32_t)k[11]<<24));
+      mix(a,b,c);
+      k += 12; len -= 12;
+   }
+
+   /*------------------------------------- handle the last 11 bytes */
+   c += length;
+   switch(len)              /* all the case statements fall through */
+   {
+   case 11: c+=((uint32_t)k[10]<<24);
+   case 10: c+=((uint32_t)k[9]<<16);
+   case 9 : c+=((uint32_t)k[8]<<8);
+      /* the first byte of c is reserved for the length */
+   case 8 : b+=((uint32_t)k[7]<<24);
+   case 7 : b+=((uint32_t)k[6]<<16);
+   case 6 : b+=((uint32_t)k[5]<<8);
+   case 5 : b+=k[4];
+   case 4 : a+=((uint32_t)k[3]<<24);
+   case 3 : a+=((uint32_t)k[2]<<16);
+   case 2 : a+=((uint32_t)k[1]<<8);
+   case 1 : a+=k[0];
+     /* case 0: nothing left to add */
+   }
+   mix(a,b,c);
+   /*-------------------------------------------- report the result */
+   return(c);
+}
+
+/********************************************************************/
+
+struct collchain_t
+{
+struct collchain_t *next;
+void *payload;
+uint32_t fullhash, length;
+unsigned char mem[1];
+};
+
+
+void **JenkinsIns(void *base_i, unsigned char *mem, uint32_t length)
+{
+struct collchain_t ***base = (struct collchain_t ***)base_i;
+uint32_t hf, h;
+struct collchain_t **ar;
+struct collchain_t *chain, *pchain;
+
+if(!*base)
+	{
+	*base = calloc(1, hashsize(J_HASHSIZE) * sizeof(void *));
+	}
+ar = *base;
+
+h = (hf = j_hash(mem, length, hashsize(J_HASHSIZE))) & hashmask(J_HASHSIZE);
+pchain = chain = ar[h];
+while(chain)
+	{
+	if((chain->fullhash == hf) && (chain->length == length) && !memcmp(chain->mem, mem, length))
+		{
+		if(pchain != chain) /* move hit to front */
+			{
+			pchain->next = chain->next;
+			chain->next = ar[h];
+			ar[h] = chain;
+			}
+		return(&(chain->payload));
+		}
+	pchain = chain;
+	chain = chain->next;
+	}
+
+chain = calloc(1, sizeof(struct collchain_t) + length - 1);
+memcpy(chain->mem, mem, length);
+chain->fullhash = hf;	
+chain->length = length;	
+chain->next = ar[h];
+ar[h] = chain;
+return(&(chain->payload));
+}
+
+
+void JenkinsFree(void *base_i)
+{
+struct collchain_t ***base = (struct collchain_t ***)base_i;
+uint32_t h;
+struct collchain_t **ar;
+struct collchain_t *chain, *chain_next;
+
+if(base && *base)
+	{
+	ar = *base;
+	for(h=0;h<hashsize(J_HASHSIZE);h++)
+		{
+		chain = ar[h];
+		while(chain)
+			{
+			chain_next = chain->next;
+			free(chain);
+			chain = chain_next;
+			}
+		}
+
+	free(*base);
+	*base = NULL;
+	}
+}
+
+#endif
